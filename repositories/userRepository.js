818bfca91f2f4ee0981b34c13366ee74d6e5aac8
Janiru <janiru160@gmail.com>
2022-01-22 15:25:23 +0530

    fix challenge 0.a

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -7,23 +7,24 @@ function init(db) {
   _db = db;
 }
 
-const knex_db = require('../db/db-config')
+const knex_db = require("../db/db-config");
 
 function getUserByEmailAndPassword(email, password) {
   const sql = `SELECT id, name, email, password FROM users WHERE email = ?`;
 
   return new Promise(async (resolve, reject) => {
-    knex_db.raw(sql, [email])
+    knex_db
+      .raw(sql, [email])
       .then((data) => {
         if (bcrypt.compareSync(password, data[0].password)) {
           resolve(data[0]);
         } else {
-          reject("Password Mismatch");
+          reject("User authentication failed");
         }
       })
       .catch((error) => {
-        reject("User Not Found");
-      })
+        reject("User authentication failed");
+      });
   });
 }
 
@@ -31,18 +32,18 @@ function getUserById(id) {
   const sql = `SELECT id, name, email, password FROM users WHERE id = ?`;
 
   return new Promise((resolve, reject) => {
-    knex_db.raw(sql, [id])
+    knex_db
+      .raw(sql, [id])
       .then((user) => {
-        resolve(user[0])
+        resolve(user[0]);
       })
       .catch((error) => {
-        reject(error)
-      })
+        reject(error);
+      });
   });
 }
 
 function signUpUser(name, email, passwordOne, passwordTwo) {
-
   const sql1 = `SELECT id, name, email, password FROM users WHERE email = ?`;
 
   return new Promise(async (resolve, reject) => {
@@ -57,31 +58,33 @@ function signUpUser(name, email, passwordOne, passwordTwo) {
       data.error = "values missing";
       reject(data);
     } else {
-      knex_db.raw(sql1, [email])
+      knex_db
+        .raw(sql1, [email])
         .then(async (user) => {
           if (!(user[0] == undefined)) {
             data.error = "Already Registered";
-            reject(data)
+            reject(data);
           } else {
             if (passwordOne != passwordTwo) {
               data.error = "Passwords Mismatch";
-              reject(data)
+              reject(data);
             } else {
               const hashPassword = await bcrypt.hash(passwordTwo, 10);
               const sql = `INSERT INTO users(id, name, email, password, country_currency) VALUES(NULL,?,?,?,?)`;
-              knex_db.raw(sql, [name, email, hashPassword,"LKR"])
+              knex_db
+                .raw(sql, [name, email, hashPassword, "LKR"])
                 .then(() => {
                   resolve();
                 })
                 .catch((error) => {
-                  reject(error)
-                })
+                  reject(error);
+                });
             }
           }
         })
         .catch((error) => {
-          reject(error)
-        })
+          reject(error);
+        });
     }
   });
 }
@@ -90,19 +93,19 @@ function getUserCountryFlag(id) {
   const sql = `SELECT country_currency FROM users WHERE id = ?`;
 
   return new Promise((resolve, reject) => {
-    knex_db.raw(sql, [id])
+    knex_db
+      .raw(sql, [id])
       .then(async (user) => {
-
-        const url = `https://restcountries.com/v2/currency/${user[0].country_currency}`
+        const url = `https://restcountries.com/v2/currency/${user[0].country_currency}`;
 
         const response = await fetch(url);
         const data = await response.json();
 
-        resolve(data[0].flag)
+        resolve(data[0].flag);
       })
       .catch((error) => {
-        reject(error)
-      })
+        reject(error);
+      });
   });
 }
 
@@ -111,5 +114,5 @@ module.exports = {
   getUserById,
   signUpUser,
   init,
-  getUserCountryFlag
+  getUserCountryFlag,
 };
